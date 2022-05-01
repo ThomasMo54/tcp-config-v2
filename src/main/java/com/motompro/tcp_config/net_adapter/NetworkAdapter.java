846f1482a28c721e99a75689a6c883ac00bf9055
TomMo54 <tmoretti@motompro.com>
2022-05-01 13:29:29 +0200

    feat: Added update methods to NetworkAdapter class

--- src/main/java/com/motompro/tcp_config/net_adapter/NetworkAdapter.java
+++ src/main/java/com/motompro/tcp_config/net_adapter/NetworkAdapter.java
@@ -1,6 +1,11 @@
 package com.motompro.tcp_config.net_adapter;
 
+import java.io.IOException;
+
 public class NetworkAdapter {
+
+    private static final String INTERFACE_SET_IP_SCRIPT_FILE_NAME = "NetInterfaceSetIp.exe";
+    private static final String INTERFACE_SET_GATEWAY_SCRIPT_FILE_NAME = "NetInterfaceSetGateway.exe";
 
     private final String displayName;
     private final String name;
@@ -16,5 +21,13 @@ public class NetworkAdapter {
 
     public String getName() {
         return name;
+    }
+
+    public void setIPAddress(String ip, String mask) throws IOException {
+        Runtime.getRuntime().exec("D:\\temp\\" + INTERFACE_SET_IP_SCRIPT_FILE_NAME + name + " " + ip + " " + mask);
+    }
+
+    public void setGateway(String gateway) throws IOException {
+        Runtime.getRuntime().exec("D:\\temp\\" + INTERFACE_SET_GATEWAY_SCRIPT_FILE_NAME + name + " " + gateway);
     }
 }
